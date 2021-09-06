c91479f2c71d6a307cb4d51fed1332af760f4a72
NATTHASITH VORANITIYAOWAPA <61766926+ohmspeed777@users.noreply.github.com>
2021-09-06 16:09:10 +0700

    Initial commit

---
+++ src/SingletonClient.java
@@ -0,0 +1,10 @@
+public class SingletonClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singleton singleton = Singleton.getInstance();
+		System.out.print(singleton.getDescription());
+		
+	}
+
+}
